99791fcd070e7c243634d774808709fe2947cbcb
slinton2026S <sophielintonn7@gmail.com>
2026-09-04 10:33:55 -0400

    I am Sophie

---
+++ SophieLinton.java
@@ -0,0 +1,16 @@
+public class SophieLinton
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+    /**
+     * My name.
+     * 
+     * @param args Argument
+     */
+    public static void main(String[] args) {
+        System.out.println("Sophie Linton is on the team.");
+    }
+}
