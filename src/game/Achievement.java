c354deb91ea6af506275c75b8ce0eccf0eaaa5a7
annamariarS2026 <msannam007@gmail.com>
2026-09-18 11:23:41 -0400

    Achievement class method stubs
    method stubs

--- src/game/Achievement.java
+++ src/game/Achievement.java
@@ -1,4 +1,5 @@
 package game;
+
 /**
  * Creating a few achievements for players to unlock in the game.
  *
@@ -8,10 +9,61 @@ package game;
 
 public class Achievement
 {
-    //~ Fields ................................................................
+    // ----------------------------------------------------------
+    /**
+     * Create a new Achievement object.
+     * 
+     * @param x
+     *            is a parameter
+     * @param y
+     *            is also a parameter
+     */
+    // ~ Fields ................................................................
 
-    //~ Constructors ..........................................................
+    // ~ Constructors ..........................................................
+    public Achievement(String x, String y)
+    {
+        // idk what to call the variables
+    }
+    // ----------------------------------------------------------
+
+    // ~Public Methods ........................................................
+
+    /**
+     * Returns as string of the achievement name 
+     * @return string achievement name
+     */
+    public String getName()
+    {
+        return null;
+    }
     
-    //~Public  Methods ........................................................
-
+    // ----------------------------------------------------------
+    /**
+     * returns a string of the achievement description and what the player needs to do
+     * @return a string of the achievement description
+     */
+    public String getDescription()
+    {
+        return null;
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Returns a true false of unlocked status
+     * @return boolean of unlocked status
+     */
+    public boolean isUnlocked()
+    {
+        return false;
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Changes status to true/false of unlocked for the specific achievement
+     */
+    public void unlock()
+    {
+        //empty
+    }
 }
