b11b4aed1c16f924001acbfa0dc0e412e1a9cbf3
sebaspach24 <sebaspach24@vt.edu>
2026-09-18 10:22:23 -0400

    Update Achievement.java

--- src/Achievement.java
+++ src/Achievement.java
@@ -1,9 +1,17 @@
+/**
+ * Represents a movie in the movie collection.
+ * A Movie inherits its title, genre, rating, and year from MovieADT.
+ *
+ * @author Sebastian Pacheco
+ * @version 2026.09.10
+ */
+
 public class Achievement
 {
     //~ Fields ................................................................
 
     //~ Constructors ..........................................................
-
+    
     //~Public  Methods ........................................................
 
 }
