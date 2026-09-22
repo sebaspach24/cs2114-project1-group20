ad3227edf9dcc3dcedf08d209ac2183319afea25
annamariarS2026 <msannam007@gmail.com>
2026-09-22 16:04:55 -0400

    achievement test methods and adding student to the buildpath
    i added student.jar and cs2 support because that's what it said to do so hopefully it doesn't break anything

--- src/game/AchievementTest.java
+++ src/game/AchievementTest.java
@@ -1,10 +1,61 @@
 package game;
+// -------------------------------------------------------------------------
+/**
+ *  Tests achievement class
+ * 
+ *  @author srena
+ *  @version Sep 22, 2026
+ */
 public class AchievementTest
 {
-    //~ Fields ................................................................
 
-    //~ Constructors ..........................................................
+    
+    
+    
+    // ----------------------------------------------------------
+    /**
+     * tests that an achievement object is created properly 
+     */
+    public void testAchievement()
+    {
+        //
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Creates an achievement object and tests if the name is properly returned 
+     */
+    public void testGetName()
+    {
+        //
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Creates an achievement object and checks if the description is properly returned
+     */
+    public void testGetDescription()
+    {
+        //
+    }
 
-    //~Public  Methods ........................................................
+    // ----------------------------------------------------------
+    /**
+     * Checks if the achievement is registered as unlocked when a hypothetical player object "unlocks" that achievement 
+     */
+    public void testIsUnlocked()
+    {
+        //
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Tests whether the unlock registers when called.
+     */
+    public void testUnlock()
+    {
+        //
+    }
 
+    
 }
